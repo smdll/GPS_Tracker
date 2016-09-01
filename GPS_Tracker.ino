ae69ae030985d1e0872a010ee017c63ecd115b22
Song <songmingda1234@hotmail.com>
2016-09-01 21:00:01 +0800

    modified:   GPS_Tracker.ino

--- GPS_Tracker.ino
+++ GPS_Tracker.ino
@@ -1,14 +1,19 @@
-#include "u8glib.h"
+#include "U8glib.h"
 
-String Re;
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE | U8G_I2C_OPT_DEV_0); // I2C / TWI
+int inp = 0;
 
 void setup()
 {
   Serial.begin(9600);
+  u8g.setColorIndex(1);
+  u8g.setFont(u8g_font_unifont);
 }
 
 void loop()
 {
-  while(Serial.available() > 0)
-    Re = Serial.readString();
+  u8g.firstPage();
+  while (Serial.available() > 0)
+    inp = Serial.read();
+  do u8g.drawStr( 0, 22, (char*)inp); while (u8g.nextPage());
 }
